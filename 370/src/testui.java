ba3e8589ee61ade8166615090fc9f0dc966c8b81
svoss <svossler7@gmail.com>
2023-09-05 11:33:23 -0400

    inputs done i think

--- 370/src/testui.java
+++ 370/src/testui.java
@@ -1,25 +1,24 @@
 import processing.core.*;//this is the library that makes display very easy
 /*
 To Do List:
-calculate cuts button
+calculate cuts button interactivity
 part display (right side)
 Stock Display (right side)
 window resizing compatibility
-blade thickness box
-grain direction input boxes (currently cannont be changed)
-limit framerate
-test
+limit framerate?
+
 could swap to part objects instead of box/txt array but the only advantage is adding more parts easily
 the problem is if i add more parts i need to redo the display to add a couple more or add a scroll wheel to allow more than like 10 parts.
+might have to use objects for the display on the right side, as the maximum number of parts is 7*max quantity (prolly at least 700 total)
 */
 
 public class testui extends PApplet{
 int rows=7;
-Boolean grain[]= {false,false,true,true,false,true,false,false};//x8 false=horizontal
+Boolean grain[]= {false,false,false,false,false,false,false,false,false};//x9 false=horizontal grain[7]=stock grain[8]=t/f if grain direction matters
 Boolean box[]= {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};//x21 
-String	txt[]= {"test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test","test"};//x21
-String stock[]= {"w","h","q"};
-Boolean Stockbox[]= {false,false,false};
+String	txt[]= {"w","h","q","w","h","q","w","h","q","w","h","q","w","h","q","w","h","q","w","h","q"};//x21
+String stock[]= {"w","h","q","cutwidth"};
+Boolean Stockbox[]= {false,false,false,false};
 int cooldown=0;//frames b4 another input is processed
 int selectedboxcolor=250;
 int i=0;
@@ -27,10 +26,9 @@ int i=0;
     public void settings() {
         size(1200, 675);//sets window size (will effect other display elements)
     }
-
     @Override
     public void draw() {
-        background(0);
+        background(50);
         fill(190);
         rect(0, 0, width/3, height);//left bar background
         textSize(20);	//prolly should determine this based on screen size but whatev
@@ -66,9 +64,6 @@ int i=0;
             fill(10);
            	text(txt[2+(3*i)], 220, 100+(40*i));
         }
-        //add confirm button and grain toggle here
-        
-        
         for (i=0;i<rows;i++) {//loop for grain direction boxes
             if(grain[i]) {
                 fill(200);//vertical display
@@ -88,9 +83,9 @@ int i=0;
                 fill(200);//vertical display
                 rect(300,430,70,-30);
                 fill(10);
-                rect(300,0,0,0);
+                rect(318,430,10,-30);
                 fill(10);
-                rect(300,0,0,0);
+                rect(352,430,-10,-30);
                 }else {
             	fill(200);//horizontal display
             	rect(300,430,70,-30);
@@ -127,7 +122,16 @@ int i=0;
             rect(220,430,70,-30);
             }
         fill(10);
-       	text(stock[2], 220, 420);// end of Stock display
+       	text(stock[2], 220, 420);
+        if(Stockbox[3]) {
+            fill(selectedboxcolor);
+            rect(60,470,70,-30);
+        }else {
+            fill(200);
+            rect(60,470,70,-30);
+        }
+        fill(10);
+        text(stock[3], 60, 460);// end of Stock display
        	//+ and - buttons for Parts
        	if(rows==7) {
        	fill(250,0,0);//make the + button red when you are at max rows
@@ -146,6 +150,21 @@ int i=0;
         textSize(15);
        	fill(10);
        	text("Add/Remove Parts",120,50);
+           //toggle grain button
+        if(grain[8]) {
+            fill(0,250,0);
+        }else fill(250,0,0);//make the - button red when grain is off
+        rect(300,20,50,50);
+        textSize(12);
+        fill(10);
+        if(grain[8]) {
+            text("Grain On", 300, 50);
+        }else    text("Grain Off", 300, 50);
+        //calculate button
+        fill(200);
+        rect(300,600,50,50);
+        fill(10);
+        text("Calculate", 300, 625);
     	update();
     }
 	public static void main(String[] args) {
@@ -164,20 +183,29 @@ if(cooldown<1&&mousePressed){//check if they clicked on a text box or if they cl
          if(mouseX>220&&mouseX<290&&mouseY<(110+(40*i))&&mouseY>(80+(40*i))) {
         	 box[2+(i*3)]=true;	
 	 } else box[2+(i*3)]=false;
+         //check for grain direction
+         if(mouseX>300&&mouseX<370&&mouseY<(110+(40*i))&&mouseY>(80+(40*i))) grain[i]= !grain[i];//flip bool
      }
 	 //mouse collision for each Stock box
-	 if(mouseX>60&&mouseX<130&&mouseY<(430)&&mouseY>(400)) {
+	 if(mouseX>60&&mouseX<130&&mouseY<430&&mouseY>400) {
     	 Stockbox[0]=true;	
  } else Stockbox[0]=false;
-	 if(mouseX>140&&mouseX<210&&mouseY<(430)&&mouseY>(400)) {
+	 if(mouseX>140&&mouseX<210&&mouseY<430&&mouseY>400) {
     	 Stockbox[1]=true;	
  } else Stockbox[1]=false;
-	 if(mouseX>220&&mouseX<290&&mouseY<(430)&&mouseY>(400)) {
+	 if(mouseX>220&&mouseX<290&&mouseY<430&&mouseY>400) {
     	 Stockbox[2]=true;	
  } else Stockbox[2]=false;
+    if(mouseX>60&&mouseX<130&&mouseY<470&&mouseY>440) {
+        Stockbox[3]=true;
+    } else Stockbox[3]=false;
+     //stock grain direction
+    if(mouseX>300&&mouseX<370&&mouseY<430&&mouseY>400)grain[7]=!grain[7];
 	//check for + or - maxParts button
-	 if(rows<7&&mouseX>60&&mouseX<110&&mouseY<(70)&&mouseY>(20)) rows++;
-	 if(rows>1&&mouseX>240&&mouseX<290&&mouseY<(70)&&mouseY>(20)) rows--;
+	 if(rows<7&&mouseX>60&&mouseX<110&&mouseY<70&&mouseY>20) rows++;
+	 if(rows>1&&mouseX>240&&mouseX<290&&mouseY<70&&mouseY>20) rows--;
+    //grain top toggle check
+    if(mouseX>300&&mouseX<350&&mouseY<70&&mouseY>20) grain[8]= !grain[8];
 }
 //now that we have checked what box might be selected, handle typing into boxes
 if (keyPressed&&cooldown<1) {
@@ -199,6 +227,10 @@ if (keyPressed&&cooldown<1) {
                 stock[2]="";
                 cooldown=10;
             }
+            if(Stockbox[3]&&cooldown<1){
+                stock[3]="";
+                cooldown=10;
+            }
 		}else {
 			if(key=='0'||key=='1'||key=='2'||key=='3'||key=='4'||key=='5'||key=='6'||key=='7'||key=='8'||key=='9') {//add a check to only allow numerical inputs
                 if (box[i] && cooldown < 1){
@@ -215,6 +247,10 @@ if (keyPressed&&cooldown<1) {
                 }
                 if (Stockbox[2] && cooldown < 1){
                     stock[2] += key;
+                    cooldown=10;
+                }
+                if (Stockbox[3] && cooldown < 1){
+                    stock[3] += key;
                     cooldown=10;
                 }
             }
