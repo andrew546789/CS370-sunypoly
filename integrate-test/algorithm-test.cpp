dc0789b423d77894388610a8bcb4131a03848576
andrew546789 <121363902+andrew546789@users.noreply.github.com>
2023-09-06 12:30:32 -0400

    Add files via upload
    alot of random stuff i need to label better. its a mix of algorithms i attempted to borrow (with credit) and the test algorithm that can be fed to the ui

---
+++ integrate-test/algorithm-test.cpp
@@ -0,0 +1,15 @@
+// test function and a driver to test it
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void testalgorithm()
+{
+    cout << "hi";
+}
+
+int main()
+{
+    testalgorithm();
+    return 0;
+}
