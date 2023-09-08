9dc5b56878f1fb44b8587e2a79d1b7c46d2afbde
andrew546789 <121363902+andrew546789@users.noreply.github.com>
2023-09-08 17:06:41 -0400

    Add files via upload
    passing the array works  (i tested with a predefined array) but filling up out is somehow still a problem. bugfixing needed.

--- integrate-test/transfer.cpp
+++ integrate-test/transfer.cpp
@@ -8,16 +8,21 @@ using namespace std;
 int main()
 {
     int row = 4, i=0, alen=0;
-    float *a;
     float stock[3] = {10,10,3};
     float cut[12] = {2,10,1,3,10,1,3,5,1,4,4,1};
-    a = cry(stock,cut,row);
+    float* a = cry(stock,cut,row);
     cout << "pass" << endl;
 
-    alen = (sizeof(a)/ sizeof(a[0]));
-    cout << "size:" << alen << endl << endl;
+    int temps=0;
+    while(a[temps]!=NULL)
+    {
+        temps++;
+    }
 
-    for(i=0;i<alen;i++)
+
+    cout << "size=" << temps << endl;
+
+    for(i=0;i<temps;i++)
     {
         cout << a[i] << endl;
     }
