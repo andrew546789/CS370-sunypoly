4683d85078503906504dc22a35302e3ee4cba9ec
andrew546789 <121363902+andrew546789@users.noreply.github.com>
2023-09-11 12:52:59 -0400

    Add files via upload
    make a function to call a function

--- transfer.cpp
+++ transfer.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <fstream>
 #include "transferfunc.cpp"
-#include <stack>
-
 using namespace std;
 
-int main()
+float call(float cut[], float stock[], int row)
 {
-    int row = 4, i=0, alen=0;
-    float stock[3] = {10,10,3};
-    float cut[12] = {2,10,1,3,10,1,3,5,1,4,4,1};
+    int i=0, alen=0;
+    //int row=4;
+    //float stock[3] = {10,10,3};
+    //float cut[12] = {2,10,1,3,10,1,3,5,1,4,4,1};
     float* a = cry(stock,cut,row);
     alen = totalcut(cut,row);
 
