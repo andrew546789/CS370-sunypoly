80a2b72da8ee29315e5c8e33840b173765991b5e
ZoilaLoja <lojagz@sunypoly.edu>
2023-09-19 19:29:00 -0400

    Button alignment & documentation

--- 370/src/NewUI.java
+++ 370/src/NewUI.java
@@ -5,29 +5,38 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewUI{
+public class NewUI {
     private JFrame frame;
-    private JPanel inputPanel;
-    private JPanel buttonPanel;
-    private JPanel displayPanel;
+    private JPanel inputPanel; // Main panel for user inputs
+    private JPanel buttonPanel; // Panel for buttons
+    private JPanel displayPanel; // Panel for displaying rectangles
     private JButton addButton;
     private JButton drawButton;
     private JButton eraseButton;
     private List<RectangleInputPanel> rectangleInputPanels = new ArrayList<>();
 
     public NewUI() {
+        // Create the main frame
         frame = new JFrame("Multiple Rectangle Drawer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 400);
 
+        // Create the main input panel with a vertical BoxLayout
         inputPanel = new JPanel();
         inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
 
-        buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT)); // Use FlowLayout for buttons
-        addButton = new JButton("Add Rectangle Input");
-        drawButton = new JButton("Draw Rectangles");
-        eraseButton = new JButton("Erase Rectangles");
+        // Create a sub-panel for buttons with a horizontal FlowLayout
+        buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT)); // Arrange buttons from left to right
 
+        // Create buttons and set their preferred sizes
+        addButton = new JButton("Add");
+        eraseButton = new JButton("Remove");
+        drawButton = new JButton("Calculate");
+        addButton.setPreferredSize(new Dimension(120, 30)); // Set button size
+        drawButton.setPreferredSize(new Dimension(120, 30)); // Set button size
+        eraseButton.setPreferredSize(new Dimension(120, 30)); // Set button size
+
+        // Add action listeners to buttons
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -49,10 +58,12 @@ public class NewUI{
             }
         });
 
+        // Add buttons to the button panel
         buttonPanel.add(addButton);
         buttonPanel.add(drawButton);
         buttonPanel.add(eraseButton);
 
+        // Create a panel for displaying rectangles and customize its paint behavior
         displayPanel = new JPanel() {
             @Override
             protected void paintComponent(Graphics g) {
@@ -61,12 +72,18 @@ public class NewUI{
             }
         };
 
-        inputPanel.add(buttonPanel); // Add the button panel at the top
+        // Add the button panel at the top of the input panel
+        inputPanel.add(buttonPanel);
+
+        // Add input panel to the left side of the main frame and display panel to the center
         frame.add(inputPanel, BorderLayout.WEST);
         frame.add(new JScrollPane(displayPanel), BorderLayout.CENTER);
+
+        // Make the frame visible
         frame.setVisible(true);
     }
 
+    // Method to add a new RectangleInputPanel to the input panel
     private void addRectangleInputPanel() {
         RectangleInputPanel rectangleInputPanel = new RectangleInputPanel(rectangleInputPanels.size() + 1);
         rectangleInputPanels.add(rectangleInputPanel);
@@ -74,12 +91,14 @@ public class NewUI{
         inputPanel.revalidate();
     }
 
+    // Method to draw rectangles on the display panel
     private void drawRectangles(Graphics g) {
         for (RectangleInputPanel inputPanel : rectangleInputPanels) {
             inputPanel.drawRectangle(g);
         }
     }
 
+    // Method to clear all rectangles and input panels
     private void clearRectangles() {
         rectangleInputPanels.clear();
         inputPanel.removeAll();
@@ -88,6 +107,7 @@ public class NewUI{
         displayPanel.repaint();
     }
 
+    // Main method to create and run the UI
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
@@ -97,6 +117,7 @@ public class NewUI{
         });
     }
 
+    // Inner class for user input panels
     private class RectangleInputPanel extends JPanel {
         private JTextField heightField;
         private JTextField widthField;
@@ -123,6 +144,7 @@ public class NewUI{
             quantityField.setPreferredSize(new Dimension(40, 25));
         }
 
+        // Method to draw a rectangle based on user input
         public void drawRectangle(Graphics g) {
             String heightStr = heightField.getText();
             String widthStr = widthField.getText();
