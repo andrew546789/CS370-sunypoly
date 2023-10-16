c8e99003205df0705da4d946ed2f7c41213cf0b3
ZoilaLoja <lojagz@sunypoly.edu>
2023-10-16 12:33:40 -0400

    Accordion + split screen

--- 370/src/CollapsibleAccordionExample.java
+++ 370/src/CollapsibleAccordionExample.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class CollapsibleAccordionExample {
     public static void main(String[] args) {
@@ -19,34 +17,12 @@ public class CollapsibleAccordionExample {
         JPanel leftContentPanel = new JPanel();
         leftContentPanel.setLayout(new BorderLayout());
 
-        JPanel rightPanel = new JPanel();
-        rightPanel.setBackground(Color.lightGray);
-
-        JToggleButton toggleButton = new JToggleButton("Toggle Accordion");
+       // JToggleButton toggleButton = new JToggleButton("Toggle Accordion");
         JPanel accordionPanel = new JPanel();
         accordionPanel.setLayout(new BorderLayout());
-        JLabel accordionLabel = new JLabel("Accordion Content");
-        accordionPanel.add(accordionLabel);
 
         JComboBox<String> grainComboBox = new JComboBox<>(new String[]{"On", "Off"});
         JTextField kerfSizeField = new JTextField();
-
-        toggleButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (toggleButton.isSelected()) {
-                    String grainInput = (String) grainComboBox.getSelectedItem();
-                    String kerfSizeInput = kerfSizeField.getText();
-                    accordionLabel.setText("Grain: " + grainInput + ", Kerf Size: " + kerfSizeInput);
-                    accordionPanel.setVisible(true);
-                } else {
-                    accordionLabel.setText("Accordion Content");
-                    accordionPanel.setVisible(false);
-                }
-            }
-        });
-
-        accordionPanel.setVisible(false);
 
         JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new GridLayout(2, 2));
@@ -56,15 +32,12 @@ public class CollapsibleAccordionExample {
         inputPanel.add(kerfSizeField);
 
         leftContentPanel.add(new JLabel("Left Panel Content"), BorderLayout.NORTH);
-        leftContentPanel.add(toggleButton, BorderLayout.CENTER);
+       // leftContentPanel.add(toggleButton, BorderLayout.CENTER);
         leftContentPanel.add(inputPanel, BorderLayout.SOUTH);
 
         leftPanel.add(leftContentPanel, BorderLayout.WEST);
-        leftPanel.add(rightPanel, BorderLayout.CENTER);
 
         frame.add(leftPanel, BorderLayout.WEST);
         frame.setVisible(true);
     }
 }
-
-
---
+++ 370/src/SplitScreenExample.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SplitScreenExample {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Split Screen Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+
+        // Create the main panel that divides the screen into left and right panels
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        // Create the left panel that is wider and further divides into top and bottom halves
+        JPanel leftPanel = new JPanel(new GridLayout(2, 1));
+        JPanel topLeftPanel = new JPanel();
+        JPanel bottomLeftPanel = new JPanel();
+
+        // Create components for kerf width and grain
+        JLabel kerfLabel = new JLabel("Kerf Width: ");
+        JTextField kerfField = new JTextField(10);
+        JLabel grainLabel = new JLabel("Grain: ");
+        JComboBox<String> grainComboBox = new JComboBox<>(new String[]{"On", "Off"});
+
+        // Add components to the bottomLeftPanel
+        bottomLeftPanel.setLayout(new BorderLayout());
+        JPanel kerfGrainPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        kerfGrainPanel.add(kerfLabel);
+        kerfGrainPanel.add(kerfField);
+        kerfGrainPanel.add(grainLabel);
+        kerfGrainPanel.add(grainComboBox);
+        bottomLeftPanel.add(kerfGrainPanel, BorderLayout.SOUTH);
+
+        // Create the right panel
+        JPanel rightPanel = new JPanel();
+
+        // Set the preferred size of the left panel to make it wider
+        leftPanel.setPreferredSize(new Dimension(400, 600));
+
+        // Customize the panels with background colors
+        topLeftPanel.setBackground(Color.RED);
+        bottomLeftPanel.setBackground(Color.BLUE);
+        rightPanel.setBackground(Color.GREEN);
+
+        // Add the left and right panels to the main panel
+        mainPanel.add(leftPanel, BorderLayout.WEST);
+        mainPanel.add(rightPanel, BorderLayout.CENTER);
+
+        // Add the top and bottom halves to the left panel
+        leftPanel.add(topLeftPanel);
+        leftPanel.add(bottomLeftPanel);
+
+        // Add the main panel to the frame
+        frame.add(mainPanel);
+
+        frame.setVisible(true);
+    }
+}
